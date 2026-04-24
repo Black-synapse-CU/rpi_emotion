a0205bc95055caf99b3249edab2ee7c8a4506469
CMBORJAS <christian.mandujanoborjas@ucdenver.edu>
2026-04-24 04:02:58 -0600

    Initial commit: Raspberry Pi Emotion Recognition project

---
+++ push_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Ensure we are in a git repo
+git init
+# Set the remote
+git remote remove origin 2>/dev/null
+git remote add origin https://github.com/Black-synapse-CU/rpi_emotion.git
+# Stage and commit
+git add .
+git commit -m "Initial commit: Raspberry Pi Emotion Recognition project"
+# Push
+git push -u origin main

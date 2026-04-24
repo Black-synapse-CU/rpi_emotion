7d1dba28ad9473e99af3666bbe285c7bd4e4e245
CMBORJAS <christian.mandujanoborjas@ucdenver.edu>
2026-04-24 04:15:30 -0600

    reworked for easier push

--- push_project.sh
+++ push_project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ensure we are in a git repo
 git init
-# Set the remote
+# Set the remote (uses the authenticated URL from .git/config if already set)
 git remote remove origin 2>/dev/null
 git remote add origin https://github.com/Black-synapse-CU/rpi_emotion.git
 # Stage and commit
